03748d57e24bbdef966e0cc7431335166543d7a9
Pete Stewart <petestew@gmail.com>
2020-06-21 09:30:16 -0500

    converted to webpack

---
+++ src/javascripts/main.js
@@ -0,0 +1,66 @@
+import '../styles/main.scss';
+import 'bootstrap';
+
+const projects = [
+  {
+    title: 'Cool Project1',
+    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
+    description: 'This is the best project',
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: true,
+    url: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+  },
+  {
+    title: 'Cool Project2',
+    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
+    description: 'This is the best project',
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: true,
+    url: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+  },
+  {
+    title: 'Cool Project3',
+    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
+    description: 'This is the best project',
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: true,
+    url: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+  },
+  {
+    title: 'Cool Project4',
+    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
+    description: 'This is the best project',
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: true,
+    url: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+  },
+];
+
+const printToDom = (elementID, content) => {
+  const selectedDiv = document.querySelector(elementID);
+  selectedDiv.innerHTML = content;
+};
+
+const createProjectCards = (contentArray) => {
+  let domString = '';
+  for (let i = 0; i < contentArray.length; i += 1) {
+    domString += '<div class="card">';
+    domString += ` <div class="cardTitle">${projects[i].title}</div>`;
+    domString += ` <div class="cardDescription">${projects[i].description}</div>`;
+    domString += ` <div class="cardTechnologiesUsed">${projects[i].technologiesUsed}</div>`;
+    domString += ` <div class="cardLink"><a href="${projects[i].url}">WebLink</a></div>`;
+    domString += ` <div class="cardLink"><a href="${projects[i].githubUrl}">GitHub</a></div>`;
+    domString += '</div>';
+  }
+  return domString;
+};
+
+const init = () => {
+  printToDom('#projectsGrid', createProjectCards(projects));
+};
+
+init();
