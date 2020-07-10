fc9fbfbcc04dbdb4aed1e7deca115ccf8ddb0155
Pete Stewart <petestew@gmail.com>
2020-07-09 21:48:48 -0500

    firebase app deployed

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,31 +1,46 @@
 import '../styles/main.scss';
 import 'bootstrap';
-import projectsData from './helpers/projectsData';
-
-const projects = projectsData.getProjects();
+import projectsData from './helpers/data/projectsData';
 
 const printToDom = (elementID, content) => {
   const selectedDiv = document.querySelector(elementID);
   selectedDiv.innerHTML = content;
 };
 
-const createProjectCards = (contentArray) => {
-  let domString = '';
-  for (let i = 0; i < contentArray.length; i += 1) {
-    domString += '<div class="card">';
-    domString += ` <div class="cardTitle">${projects[i].title}</div>`;
-    domString += ` <div class="cardImage"><img src="${projects[i].screenshot}" alt="screenshot"></div>`;
-    domString += ` <div class="cardDescription">${projects[i].description}</div>`;
-    domString += ` <div class="cardTechnologiesUsed">${projects[i].technologiesUsed}</div>`;
-    domString += ` <div class="cardLink"><a href="${projects[i].url}">WebLink</a></div>`;
-    domString += ` <div class="cardLink"><a href="${projects[i].githubUrl}">GitHub</a></div>`;
-    domString += '</div>';
-  }
-  return domString;
-};
+const createProjectCards = () => projectsData.getProjectsData()
+  .then((projects) => {
+    let domString = '';
+    projects.forEach((project) => {
+      domString += '<div class="card">';
+      domString += ` <div class="cardTitle">${project.title}</div>`;
+      domString += ` <div class="cardImage"><img src="${project.screenshot}" alt="screenshot"></div>`;
+      domString += ` <div class="cardDescription">${project.description}</div>`;
+      domString += ` <div class="cardTechnologiesUsed">${project.technologiesUsed}</div>`;
+      domString += ` <div class="cardLink"><a href="${project.url}">WebLink</a></div>`;
+      domString += ` <div class="cardLink"><a href="${project.githubUrl}">GitHub</a></div>`;
+      domString += '</div>';
+    });
+    printToDom('#projectsGrid', domString);
+  })
+  .catch((err) => { console.error(err); });
+
+// const createProjectCards = (contentArray) => {
+//   let domString = '';
+//   for (let i = 0; i < contentArray.length; i += 1) {
+//     domString += '<div class="card">';
+//     domString += ` <div class="cardTitle">${project.title}</div>`;
+//     domString += ` <div class="cardImage"><img src="${project.screenshot}" alt="screenshot"></div>`;
+//     domString += ` <div class="cardDescription">${project.description}</div>`;
+//     domString += ` <div class="cardTechnologiesUsed">${project.technologiesUsed}</div>`;
+//     domString += ` <div class="cardLink"><a href="${project.url}">WebLink</a></div>`;
+//     domString += ` <div class="cardLink"><a href="${project.githubUrl}">GitHub</a></div>`;
+//     domString += '</div>';
+//   }
+//   return domString;
+// };
 
 const init = () => {
-  printToDom('#projectsGrid', createProjectCards(projects));
+  createProjectCards();
 };
 
 init();
